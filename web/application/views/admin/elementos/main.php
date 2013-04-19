57b0edb3753b76398dbb41eb72ad6cd6a526a217
gepb6955 <gepb6955@ML-35852.AGUAYO.local>
2013-04-19 17:49:02 -0500

    creacion theme basico administrador

--- web/application/views/admin/elementos/main.php
+++ web/application/views/admin/elementos/main.php
@@ -3,67 +3,7 @@
             <div class="section">
               <div class="section-inner clearfix">
                 <section id="content">
-                    <section id="banners">
-                            <article id="banner-distribuidores" class="block columnx2">
-                              <h2>Coo importar frutas tropicales y exÃ³ticas en europa</h2>   
-                              <div class="content">
-                                <img src="http://cloud.github.com/downloads/malsup/cycle/beach3.jpg">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sodales dui non felis scelerisque in lobortis diam blandit. Nam sit amet libero eu eros lobortis eleifend</p>
-                              <div>
-                            </article>
-                            <article id="banner-proveedores" class="block columnx2">
-                             <h2>Como exportar frutas tropicales y exÃ³ticas en europa</h2>   
-                              <div class="content">
-                                <img src="http://cloud.github.com/downloads/malsup/cycle/beach3.jpg">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sodales dui non felis scelerisque in lobortis diam blandit. Nam sit amet libero eu eros lobortis eleifend</p>
-                              <div>
-                            </article>
-                            <article id="banner-compromiso-social" class="block columnx3">
-                             <h2>Compromiso Social</h2>   
-                              <div class="content">
-                              <img src="http://cloud.github.com/downloads/malsup/cycle/beach3.jpg">                                
-                              <div>
-                            </article>
-                            <article id="banner-nuestros-productos" class="block columnx3">
-                             <h2>Disponibilidad de nuetsros produtcos</h2>   
-                              <div class="content">
-                              <img src="http://cloud.github.com/downloads/malsup/cycle/beach3.jpg">                                
-                              <div>
-                            </article>
-                             <article id="banner-nuestras-oficinas" class="block columnx3">
-                             <h2>NUestras oficinas</h2>   
-                              <div class="content">
-                              <img src="http://cloud.github.com/downloads/malsup/cycle/beach3.jpg">                                
-                              <div>
-                            </article>
-                  </section><!-- /.banners -->
-                  <section id="sidebar-last">
-                      <form class="" method="post" id="webform-contact" accept-charset="UTF-8">
-                          <h2>Comunicate con nostoros</h2> 
-                          <div class="form-item" id="webform-nombre">
-                           <input type="text" value="Nombre Completo" class="form-text required">
-                          </div>
-                          <div class="form-item" id="webform-correo-electronico">
-                           <input class="form-text required" type="email" value="Correo ElectÃ³nico">
-                          </div>
-                          <div class="form-item" id="webform-telnefono">
-                           <input type="text" value="TelÃ©fono" class="form-text required">
-                          </div>
-                          <div class="form-item" id="webform-pais">
-                            <select class="form-select">
-                              <option value="" selected="selected">peru</option>
-                              <option value="1">Colombia</option>
-                              <option value="2">chile</option>
-                            </select>
-                          </div>
-                          <div class="form-item" id="webform-mensaje">
-                            <textarea id="edit-submitted-mensaje" cols="60" rows="5" class="form-textarea required"></textarea>
-                          </div>
-                          <div class="form-actions" id="edit-actions">
-                            <input type="submit" id="edit-submit" name="op" value="Enviar" class="form-submit">
-                          </div>
-                        </form>
-                  </section>    
+                    <h1>MARCE MUY BIEN</h1>
                 </section><!-- /.content-->
               </div> <!-- /.section-inner-->
             </div> <!--/.section --> 
