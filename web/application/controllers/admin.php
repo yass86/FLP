40fdb95f1ecace38a0f7103f3eecf40fefd090b9
gepb6955 <gepb6955@ML-35852.AGUAYO.local>
2013-04-19 16:47:01 -0500

    calses para administrador de contenidos

--- web/application/controllers/admin.php
+++ web/application/controllers/admin.php
@@ -3,10 +3,9 @@ class Admin extends CI_Controller {
 	public function index()
 	{
           $cont['menu']=  $this->load->view('admin/elementos/menu',null,true);
-          $cont['contenido']=  $this->load->view('admin/elementos/contenido',null,true);
+          $cont['contenido']=  $this->load->view('admin/elementos/content',null,true);
           $cont['pie']=  $this->load->view('admin/elementos/pie',null,true);
-            echo $this->load->view('admin/administrador',$cont,true);
-                    
+            echo $this->load->view('admin/administrador',$cont,true);                    
 	}
         function precarga()
         {
