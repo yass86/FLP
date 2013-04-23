e3a2f4b2df3913264e3ecebe9ad75cb459d2d5c9
yass <yass8608@gmail.com>
2013-04-23 17:08:06 -0500

    logo

--- web/application/views/front/header.php
+++ web/application/views/front/header.php
@@ -2,7 +2,13 @@
           <div id="header-first" class="section-wrapper">
             <div class="section">
               <div class="section-inner clearfix">                   
-                         
+                <div class="block" id="block-logo">
+                    <div class="content">
+                        <a id="logo" rel="home" title="Inicio" href="<?php echo site_url()?>">
+                            <img alt="" src="<?php echo site_url('theme/images/logo.png')?>"/>
+                        </a>  
+                    </div> <!-- /.content -->
+              </div>         
               </div> <!-- /.section-inner-->
           </div> <!--/.section --> 
         </div> <!-- /.section-wrapper-->
